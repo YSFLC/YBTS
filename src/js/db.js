c4184fa5a7d599d96ac810ecaa7e2a4cce7addcd
NULLCT <cockatiel.u10@gmail.com>
2021-05-21 18:35:46 +0900

    add database class

--- src/js/db.js
+++ src/js/db.js
@@ -1,1 +1,37 @@
-var 
+'use strict';
+
+class DB {
+  constructor() {
+    // Record all books ISBN number
+    this.isbndata = new Set();
+    // Record the ISBN number of books sold
+    this.isbnsolddata = new Set();
+    // Count how many coupons are used
+    this.isbncouponcnt = 0;
+  }
+
+  sell(_input){ // If successful in selling, return true
+    if(this.isbndata.has(_input)){
+      if(this.isbnsolddata.has(_input)){ // Error of `It's already sold`
+        return false;
+      }else{ // Success!
+        this.isbnsolddata.add(_input);
+        return true;
+      }
+    }else{ // Error of `not found in isbndata`
+      return false;
+    }
+  }
+
+  addCoupon(_input){ // add couponcnt
+    this.isbncouponcnt += _input;
+  }
+
+  dump() { // Dump DB datas
+    console.log(this.isbndata);
+    console.log(this.isbnsolddata);
+    console.log(this.isbncouponcnt);
+  }
+}
+
+var db = new DB();
