1f7d172d18a6bda037ce3b644bd7d69cfb84e4a8
NULLCT <cockatiel.u10@gmail.com>
2021-06-06 23:15:34 +0900

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>aaaa</p>
+    <p> {{ error.statusCode }} </p>
+    <p> {{ error.message }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'error',
+  props: ['error']
+}
+</script>
