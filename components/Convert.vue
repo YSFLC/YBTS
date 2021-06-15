4c6c8d4cf91b79cf1f951d6784571779c68ab87a
NULLCT <cockatiel.u10@gmail.com>
2021-06-15 23:21:47 +0900

    make isbn-conver function perfect

--- components/Convert.vue
+++ components/Convert.vue
@@ -3,8 +3,8 @@
     <b-field label="ISBNTable">
       <b-input v-model="isbntabledata" type="textarea" />
     </b-field>
-    <b-button @click="convert">
-      変換
+    <b-button @click="convert()">
+      保存
     </b-button>
   </div>
 </template>
@@ -22,13 +22,20 @@ export default {
 
       for (let i of this.isbntabledata.split('\n')) {
         if (i !== '') {
-          data[String(i)]['isbn'] = 'a'
+          data[String(i)] = {}
+          data[String(i)].issold = false
+          data[String(i)].soldtime = null
         }
       }
 
-      console.log(data)
       let jsondata = JSON.stringify(data)
       console.log(jsondata)
+
+      const blob = new Blob([jsondata], { type: 'text/plain' })
+      let link = document.createElement('a')
+      link.href = window.URL.createObjectURL(blob)
+      link.download = 'isbn.json'
+      link.click()
     }
   }
 }
