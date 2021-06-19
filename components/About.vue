84a22ce72dd7f5e01820dfadfcaf3d6d4ed41d20
sasasou <agepan_19799@yahoo.co.jp>
2021-06-20 00:35:25 +0900

    aboutpagekairyou

--- components/About.vue
+++ components/About.vue
@@ -1,32 +1,50 @@
 <template>
-  <div>
-    <p>This is made by</p>
-    <h1>Y.Narita</h1>
-    <h2>K.Katayama</h2>
-    <h3>Y.Murakami</h3>
-    <h4>I.Shiotsuki</h4>
-    <h5>S.Tsukamoto</h5>
-    <h6>K.Shibuya</h6>
-    <h7>S.Sasaki</h7>
+ <div>
+  <section class="hero">
+   <div class="hero-body">
+    <p class="title">
+      About
+    </p>
+    <p class="subtitle">
+      Developer, Version, Framework
+    </p>
+   </div>
+  </section>
+<div class="tile is-ancestor">
+  <div class="tile is-4 is-vertical is-parent">
+    <div class="tile is-child box">
+      <p class="title">Developer</p>
+      <p>Y. Narita(Principal developer)</p>
+      <p>S. Sasaki(implementation)</p>
+      <p>K. Katayama(implementation)</p>
+      <p>Y. Murakami(UI-related)</p>
+      <p>I. Shiotsuki(UI-related)</p>
+      <p>K. Shibuya(adviser)</p>
+    </div>
+    <div class="tile is-child box">
+      <p class="title">Version</p>
+      <p>2021/6/15  Version1.00</p>
+    </div>
   </div>
+  <div class="tile is-parent">
+    <div class="tile is-child box">
+      <p class="title">Flamework</p>
+      <p>Vue.js          https://jp.vuejs.org/index.html</p>
+      <p>Node.js          https://nodejs.org/ja/</p>
+      <p>Nuxt.js          https://ja.nuxtjs.org/</p>
+      <p>Buefy          https://buefy.org/</p>
+      <p>Electlon          https://www.electronjs.org/</p>
+    </div>
+  </div>
+</div>
+</div>
+
 </template>
 
 <script>
-function nariNari (n) {
-  for (let i = 0; i < n; i++) {
-    console.log(String(i) + 'nari')
-  }
-}
 
-nariNari(1000)
 </script>
 
 <style scoped>
-p {
-  color:black;
-  font-size: 8mm;
-};
-h1{
-  font-size: 10cm;
-}
+
 </style>
