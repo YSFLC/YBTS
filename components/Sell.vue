f55693db0d1629497a490ca732deb98195068278
NULLCT <cockatiel.u10@gmail.com>
2021-07-12 22:24:21 +0900

    add isbnlist

--- components/Sell.vue
+++ components/Sell.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <b-field message="ISBNを入力してください">
-      <b-input placeholder="ISBN" type="search" />
-      <p class="control">
-        <b-button type="is-primary" label="追加" />
-      </p>
+      <b-input v-model="inputisbn" placeholder="ISBN" />
+      <b-button type="is-primary" label="追加" @click="addISBN" />
     </b-field>
+
+    <b-button type="is-info" label="すべて売却" @click="sell" />
+    <b-button type="is-danger" label="すべて削除" @click="removeAllISBN" />
 
     <b-table :data="sellisbn" :columns="columns" />
   </div>
@@ -21,6 +22,7 @@ export default {
   },
   data () {
     return {
+      inputisbn: null,
       sellisbn: [],
       columns: [
         {
@@ -30,9 +32,27 @@ export default {
         },
         {
           field: 'isbn',
-          label: 'label'
+          label: 'ISBN'
         }
       ]
+    }
+  },
+  methods: {
+    addISBN () {
+      this.sellisbn.push({})
+      this.sellisbn[this.sellisbn.length - 1].id = this.sellisbn.length
+      this.sellisbn[this.sellisbn.length - 1].isbn = this.inputisbn
+
+      this.inputisbn = null
+    },
+    removeAllISBN () {
+      this.sellisbn.splice(0)
+    },
+    sell () {
+      this.$buefy.toast.open({
+        message: 'あいうえお',
+        type: 'is-danger'
+      })
     }
   }
 }
