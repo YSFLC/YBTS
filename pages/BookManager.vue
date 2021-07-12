9dacd5d0560e02bce2776c8d5d2f683c6726de03
NULLCT <cockatiel.u10@gmail.com>
2021-07-12 22:57:54 +0900

    add animation

--- pages/BookManager.vue
+++ pages/BookManager.vue
@@ -4,11 +4,13 @@
       <SideBar @changePane="getPane" />
     </div>
     <div class="column m-2">
-      <DashBoard v-if="activepane === 0" />
-      <Sell v-if="activepane === 1" :json="json" />
-      <About v-if="activepane === 2" />
-      <Convert v-if="activepane === 3" />
-      <Load v-if="activepane === 4" @jsondata="getJsonData" />
+      <transition mode="out-in">
+        <DashBoard v-if="activepane === 0" />
+        <Sell v-if="activepane === 1" :json="json" />
+        <About v-if="activepane === 2" />
+        <Convert v-if="activepane === 3" />
+        <Load v-if="activepane === 4" @jsondata="getJsonData" />
+      </transition>
     </div>
   </div>
 </template>
@@ -33,4 +35,10 @@ export default {
 </script>
 
 <style scoped>
+.v-enter-active, .v-leave-active {
+  transition: opacity .1s;
+}
+.v-enter, .v-leave-to {
+  opacity: 0;
+}
 </style>
