379a3997cd127a20de018ca26a4b062d3449f05b
NULLCT <cockatiel.u10@gmail.com>
2021-07-12 20:39:00 +0900

    add props

--- components/Sell.vue
+++ components/Sell.vue
@@ -1,51 +1,25 @@
 <template>
   <div>
+    {{ jsondata }}
     <b-field message="ISBNを入力してください">
       <b-input placeholder="ISBN..." type="search" icon="book-open" />
       <p class="control">
         <b-button type="is-primary" label="追加" />
       </p>
     </b-field>
-    <b-table :data="data" :columns="columns" />
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    jsondata: {
+      type: String,
+      default: null
+    }
+  },
   data () {
     return {
-      data: [
-        { id: 1, first_name: 'Jesse', last_name: 'Simmons', date: '2016-10-15 13:43:27', gender: 'Male' },
-        { id: 2, first_name: 'John', last_name: 'Jacobs', date: '2016-12-15 06:00:53', gender: 'Male' },
-        { id: 3, first_name: 'Tina', last_name: 'Gilbert', date: '2016-04-26 06:26:28', gender: 'Female' },
-        { id: 4, first_name: 'Clarence', last_name: 'Flores', date: '2016-04-10 10:28:46', gender: 'Male' },
-        { id: 5, first_name: 'Anne', last_name: 'Lee', date: '2016-12-06 14:38:38', gender: 'Female' }
-      ],
-      columns: [
-        {
-          field: 'id',
-          label: 'ID',
-          width: '40',
-          numeric: true
-        },
-        {
-          field: 'first_name',
-          label: 'First Name'
-        },
-        {
-          field: 'last_name',
-          label: 'Last Name'
-        },
-        {
-          field: 'date',
-          label: 'Date',
-          centered: true
-        },
-        {
-          field: 'gender',
-          label: 'Gender'
-        }
-      ]
     }
   }
 }
--- pages/BookManager.vue
+++ pages/BookManager.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="column m-2">
       <DashBoard v-if="activepane === 0" />
-      <Sell v-if="activepane === 1" />
+      <Sell v-if="activepane === 1" :jsondata="json" />
       <About v-if="activepane === 2" />
       <Convert v-if="activepane === 3" />
       <Load v-if="activepane === 4" @jsondata="getJsonData" />
