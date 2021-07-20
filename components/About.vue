d56f1b41f4a99b43b864bf94552d7f90c0855a30
NULLCT <cockatiel.u10@gmail.com>
2021-07-20 20:04:54 +0900

    fix eslint warnings

--- components/About.vue
+++ components/About.vue
@@ -1,49 +1,53 @@
 <template>
- <div>
-  <section class="hero">
-   <div class="hero-body">
-    <p class="title">
-      About
-    </p>
-    <p class="subtitle">
-      Developer, Version, Framework
-    </p>
-   </div>
-  </section>
-<div class="tile is-ancestor">
-  <div class="tile is-4 is-vertical is-parent">
-    <div class="tile is-child box">
-      <p class="title">Developer</p>
-      <p>Y. Narita(Principal developer)</p>
-      <p>S. Sasaki(implementation)</p>
-      <p>K. Katayama(implementation)</p>
-      <p>Y. Murakami(UI-related)</p>
-      <p>I. Shiotsuki(UI-related)</p>
-      <p>K. Shibuya(adviser)</p>
-    </div>
-    <div class="tile is-child box">
-      <p class="title">Version</p>
-      <p>2021/6/15  Version1.00</p>
+  <div>
+    <section class="hero">
+      <div class="hero-body">
+        <p class="title">
+          About
+        </p>
+        <p class="subtitle">
+          Developer, Version, Framework
+        </p>
+      </div>
+    </section>
+    <div class="tile is-ancestor">
+      <div class="tile is-4 is-vertical is-parent">
+        <div class="tile is-child box">
+          <p class="title">
+            Developer
+          </p>
+          Y. Narita(Principal developer)<br>
+          S. Sasaki(implementation)<br>
+          K. Katayama(implementation)<br>
+          Y. Murakami(UI-related)<br>
+          I. Shiotsuki(UI-related)<br>
+          K. Shibuya(adviser)
+        </div>
+        <div class="tile is-child box">
+          <p class="title">
+            Version
+          </p>
+          <p>
+            2021/6/15 Version1.00
+          </p>
+        </div>
+      </div>
+      <div class="tile is-parent">
+        <div class="tile is-child box">
+          <p class="title">
+            Flamework
+          </p>
+          <a href="https://ja.nuxtjs.org" target="_blank">Nuxt.js</a>
+          <a href="https://buefy.org" target="_blank">buefy</a>
+          <a href="https://www.electronjs.org" target="_blank">Electlon</a>
+        </div>
+      </div>
     </div>
   </div>
-  <div class="tile is-parent">
-    <div class="tile is-child box">
-      <p class="title">Flamework</p>
-      <a href="(https://jp.vuejs.org/index.html)">Vue.js</a>
-      <a href="(https://nodejs.org/ja/)">Node.js</a>
-      <a href="(https://ja.nuxtjs.org/)">Nuxt.js</a>
-      <a href="(https://buefy.org/)">buefy</a>
-      <a href="(https://www.electronjs.org/)">Electlon</a>
-    </div>
- </div>
-</div>
-</div>
-
 </template>
 
 <script>
 </script>
 
 <style scoped>
-
 </style>
