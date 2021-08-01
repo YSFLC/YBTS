307973c44ff83135cdd036005a8a10efeeaf95a8
NULLCT <cockatiel.u10@gmail.com>
2021-08-01 22:10:25 +0900

    fix about page

--- components/About.vue
+++ components/About.vue
@@ -1,22 +1,20 @@
 <template>
   <div>
-    <section class="hero">
-      <div class="hero-body">
-        <p class="title">
-          About
-        </p>
-        <p class="subtitle">
-          Developer, Version, Framework
-        </p>
-      </div>
+    <section class="section">
+      <h1 class="title">
+        About
+      </h1>
+      <h2 class="subtitle">
+        Developer, Version, Framework
+      </h2>
     </section>
     <div class="tile is-ancestor">
-      <div class="tile is-4 is-vertical is-parent">
+      <div class="tile is-5 is-vertical is-parent">
         <div class="tile is-child box">
           <p class="title">
             Developer
           </p>
-          Y. Narita(Principal developer)<br>
+          Y. Narita(principal developer)<br>
           S. Sasaki(implementation)<br>
           K. Katayama(implementation)<br>
           Y. Murakami(UI-related)<br>
@@ -25,22 +23,20 @@
         </div>
         <div class="tile is-child box">
           <p class="title">
-            Version
+            Framework
           </p>
-          <p>
-            2021/7/20 Version1.00          クーポン以外の実装
-          </p>
+          <a href="https://nuxtjs.org" target="_blank">Nuxt.js</a>
+          <a href="https://buefy.org" target="_blank">Buefy</a>
         </div>
       </div>
       <div class="tile is-parent">
         <div class="tile is-child box">
           <p class="title">
-            Framework
+            Version
           </p>
-          <a href="https://jp.vuejs.org/" target="_blank">Vue.js</a>
-          <a href="https://ja.nuxtjs.org" target="_blank">Nuxt.js</a>
-          <a href="https://buefy.org" target="_blank">buefy</a>
-          <a href="https://www.electronjs.org" target="_blank">Electlon</a>
+          <p>
+            2021/7/20 Version1.00<br>クーポン以外の実装
+          </p>
         </div>
       </div>
     </div>
