1fe64d876134788bd1d1202ad9aa3f01e1590f56
sasasou <agepan_19799@yahoo.co.jp>
2021-08-16 14:43:09 +0900

    yoro

--- components/About.vue
+++ components/About.vue
@@ -16,19 +16,21 @@
           <p class="title">
             Developer
           </p>
-          Y. Narita(Principal developer)<br>
-          S. Sasaki(implementation)<br>
-          K. Katayama(implementation)<br>
-          Y. Murakami(UI-related)<br>
-          I. Shiotsuki(UI-related)<br>
-          K. Shibuya(adviser)
+          Y. Narita (Principal developer)<br>
+          S. Sasaki (implementation)<br>
+          K. Katayama (implementation)<br>
+          Y. Murakami (UI-related)<br>
+          I. Shiotsuki (UI-related)<br>
+          K. Shibuya (adviser)<br>
+
+          Thank you for developing this.<br>
         </div>
         <div class="tile is-child box">
           <p class="title">
             Version
           </p>
           <p>
-            2021/7/20 Version1.00          クーポン以外の実装
+            2021/7/20 Version1.0.0          クーポン以外の実装
           </p>
         </div>
       </div>
@@ -37,9 +39,9 @@
           <p class="title">
             Framework
           </p>
-          <a href="https://jp.vuejs.org/" target="_blank">Vue.js</a>
-          <a href="https://ja.nuxtjs.org" target="_blank">Nuxt.js</a>
-          <a href="https://buefy.org" target="_blank">buefy</a>
+          <a href="https://vuejs.org/" target="_blank">Vue.js</a>
+          <a href="https://nuxtjs.org" target="_blank">Nuxt.js</a>
+          <a href="https://buefy.org" target="_blank">Buefy</a>
           <a href="https://www.electronjs.org" target="_blank">Electlon</a>
         </div>
       </div>
