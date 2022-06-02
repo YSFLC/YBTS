289b5717f8aad3e6c85b7baf8841bf803633f8c8
sasasou <agepan_19799@yahoo.co.jp>
2022-06-02 16:50:13 +0900

    about teisei

--- components/About.vue
+++ components/About.vue
@@ -28,16 +28,16 @@
             Version
           </p>
           <p>2021/7/20 Version1.0.0 クーポン以外の実装</p>
-          <a href="https://nuxtjs.org" target="_blank">Nuxt.js</a>
-          <a href="https://buefy.org" target="_blank">Buefy</a>
+          <p>2022/3/30 Version1.1.0 クーポンの実装、Dashbordの改良</p>
+          <p>2022/6/2 Version1.2.0 Saveページにエクスポート機能の実装、ISBNTableにチェックボックスの実装</p>
         </div>
       </div>
       <div class="tile is-child box">
         <p class="title">
           What we used
         </p>
-        <a href="https://nuxtjs.org" target="_blank">Nuxt.js</a>
-        <a href="https://buefy.org" target="_blank">Buefy</a>
+        <a href="https://nuxtjs.org" target="_blank" rel="noopener">Nuxt.js</a>
+        <a href="https://buefy.org" target="_blank" rel="noopener">Buefy</a>
       </div>
     </div>
   </div>
