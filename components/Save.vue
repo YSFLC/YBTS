266de617d5f6a0bfc0691f2364b89fa444463014
hotep-coder <83850302+hotep-coder@users.noreply.github.com>
2022-06-02 17:53:38 +0900

    Add Exportation CSV Button (#57)
    * main
    
    * Add Export Button
    
    * Add exportation program
    
    Co-authored-by: ぬる <45321757+NULLCT@users.noreply.github.com>

--- components/Save.vue
+++ components/Save.vue
@@ -3,6 +3,9 @@
     <b-button @click="save">
       Save json
     </b-button>
+    <b-button @click="exportation">
+      Export CSVfile
+    </b-button>
   </div>
 </template>
 
@@ -17,6 +20,20 @@ export default {
   methods: {
     save () {
       const jsondata = JSON.stringify(this.json)
+
+      const blob = new Blob([jsondata], { type: 'text/plain' })
+      const link = document.createElement('a')
+      link.href = window.URL.createObjectURL(blob)
+      link.download = 'isbn.json'
+      link.click()
+    },
+    exportation () {
+      let data
+      for (const item in this.json) {
+        data = data + item + ',' + this.jason[item].issold + ',' + this.json[item].soldtime + '\n'
+      }
+
+      const jsondata = JSON.stringify(data)
 
       const blob = new Blob([jsondata], { type: 'text/plain' })
       const link = document.createElement('a')
