d895e722189d1d7bb471bb32b2ff970717306ce6
NULLCT <cockatiel.u10@gmail.com>
2022-08-31 23:45:05 +0900

    eslintrcのコンマを修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,11 +10,11 @@ module.exports = {
   },
   extends: [
     '@nuxtjs',
-    'plugin:nuxt/recommended',
+    'plugin:nuxt/recommended'
   ],
   plugins: [],
   // add your custom rules here
   rules: {
-    'vue/multi-word-component-names': 'off',
+    'vue/multi-word-component-names': 'off'
   }
 }
